11076982d19e36843da5021fd1aa7daaacfb9f70
WilliamTu <wj654cj86@gmail.com>
2022-07-24 02:00:53 +0800

    V1.1.0 優化函數庫

--- main.js
+++ main.js
@@ -1,62 +1,47 @@
-var geturl = url2array();
-var svgreg;
-window.onload = async () => {
-	window.onkeydown = function (e) {
-		keydown(e.keyCode);
-	};
-	svgreg = text2xml(await promise(openfile, 'style.svg'));
-	if (typeof geturl.st != 'undefined') {
-		inputstring.value = geturl.st;
-		transform();
-	} else {
-		svgreg.getElementsByTagName('text')[0].textContent = '字';
-		svgtopngurl(svgreg, function (url) {
-			let lk = document.querySelectorAll("[rel='icon']")[0];
-			lk.href = url;
-		});
-	}
-};
+let geturl = url2obj();
+let svgreg;
 
 function keydown(key) {
 	if (key == 13)
 		transform();
 }
 
-function transform() {
+window.onload = async () => {
+	window.onkeydown = e => keydown(e.keyCode);
+	svgreg = text2xml(await loadfile('text', 'style.svg'));
+	inputstring.value = decodeURIComponent(geturl.st) ?? '';
+	transform();
+	decode.onclick = transform;
+};
+
+async function transform() {
 	if (inputstring.value != '') {
-		svgreg.getElementsByTagName('text')[0].textContent = inputstring.value;
+		svgreg.querySelector('text').textContent = inputstring.value;
 		geturl.st = inputstring.value;
 	}
 	else {
-		svgreg.getElementsByTagName('text')[0].textContent = '字';
+		svgreg.querySelector('text').textContent = '字';
 		geturl.st = inputstring.value;
 		delete geturl.st;
 	}
-	array2url(geturl);
-	svgtopngurl(svgreg, function (url) {
-		let lk = document.querySelectorAll("[rel='icon']")[0];
-		lk.href = url;
-	});
-	svgreg.getElementsByTagName('text')[0].textContent = inputstring.value;
-	svgtoimg(svgreg, function (img) {
-		var i1 = img;
-		var i2 = document.createElement("canvas");
-		var w = i2.width = i1.naturalWidth;
-		var h = i2.height = i1.naturalHeight;
-		var n = 1;
-
-		var ctx = i2.getContext('2d');
-		ctx.drawImage(i1, 0, 0);
-		var imgdt = ctx.getImageData(0, 0, w, h);
-		var a = imgdt.data;
-		var s = '';
-		var isv = inputstring.value;
-		for (var i = 0; i < 40; i++) {
-			for (var j = 0; j < 40; j++)
-				s += a[(i * 40 * 5 + j) * 4 * 5] ? '　' : isv;
-			s += '\n';
-		}
-		outputstring.value = s;
-		ctx.putImageData(imgdt, 0, 0);
-	});
+	obj2url(geturl);
+	document.querySelector("[rel='icon']").href = await svgtopngurl(svgreg);
+	svgreg.querySelector('text').textContent = inputstring.value;
+	let i1 = await svgtoimg(svgreg);
+	let i2 = document.createElement("canvas");
+	let w = i2.width = i1.naturalWidth;
+	let h = i2.height = i1.naturalHeight;
+	let ctx = i2.getContext('2d');
+	ctx.drawImage(i1, 0, 0);
+	let imgdt = ctx.getImageData(0, 0, w, h);
+	let a = imgdt.data;
+	let s = '';
+	let isv = inputstring.value;
+	for (let i = 0; i < 40; i++) {
+		for (let j = 0; j < 40; j++)
+			s += a[(i * 40 * 5 + j) * 4 * 5] ? '　' : isv;
+		s += '\n';
+	}
+	outputstring.value = s;
+	ctx.putImageData(imgdt, 0, 0);
 }
